d7badf13b8bed6f3dede1c762556641305402b7d
angelo <aurtula1@gmail.com>
2021-02-23 13:35:39 -0500

    commit tree a772dc5966abd7010ccfa16826bd7cb723ef4922

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,46 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  numbers = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  numbers = [1, 2, 3, 4]
+  numbers << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    words = ["cat", "dog", "bug", "bird"]
+    words.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+    words = ["bug", "bots", "arrays!"]
+    cool = words.pop
+    return cool
 end
 
 def remove_element_from_start_of_array(array)
-  
+    words = ["wow", "ball", "tall"]
+    ouch = words.shift
+    return ouch
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    words = ["pm", "cm", "am"]
+    words[2] 
 end
 
 def retrieve_first_element_from_array(array)
+    words = ["wow", "am", "cool"]
+    words[0]
   
 end
 
 def retrieve_last_element_from_array(array)
+    words = ["best", "cool", "arrays!"]
+    words[-1]
   
 end
